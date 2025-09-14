6ca4f18a23218cf05b058f9b6283351d236b2bc6
Alejandro Lopez <alejandrolopez.exe@gmail.com>
2025-09-14 13:23:03 -0300

    Primer paso de auth con apple realizada

--- src/Routes/auth.php
+++ src/Routes/auth.php
@@ -7,6 +7,12 @@ use App\Models\Auth;
 use App\Models\Subscription;
 use Tuupola\Middleware\JwtAuthentication;
 
+// QUITAR
+use Slim\Routing\RouteCollectorProxy;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+// ------
+
 return function (App $app) {
   $jwtMiddleware = new JwtAuthentication([
     "secret" => $GLOBALS['config']['jwt']['secret'],
@@ -37,4 +43,32 @@ return function (App $app) {
   $app->get('/auth/mfa_check/{code}', [$authController, 'mfaCheck'])->add($jwtMiddleware);
   $app->post('/legal', [$authController, 'uploadLegalDocuments'])->add($jwtMiddleware);
   $app->get('/legal', [$authController, 'legalDocuments']);
+
+  $app->group('/auth/apple', function (RouteCollectorProxy $group) {
+    $group->post('/callback', function (Request $request, Response $response) {
+      // Esta misma vista de arriba, si preferís, renderízala con Twig o echo tal cual
+      $parsed = $request->getParsedBody() ?? [];
+      $code     = $parsed['code']     ?? null;
+      $id_token = $parsed['id_token'] ?? null;
+      $state    = $parsed['state'] ? @json_decode($parsed['state']) : null;
+      $error    = $parsed['error']    ?? null;
+
+      if(!$state){
+        $html = '<!doctype html><html><body><script>(function(){window.close()})();</script></body></html>';
+      }else{
+        $html = '<!doctype html><html><body><script>(function(){var p='.
+        json_encode([
+          'provider' => 'apple',
+          'code' => $code,
+          'id_token' => $id_token,
+          'uuid' => $state -> uuid,
+          'error' => $error,
+        ]).
+        ';try{window.opener&&window.opener.postMessage(p,"https://'. $state -> origin .'")}catch(e){}window.close()})();</script></body></html>';
+      }
+      file_put_contents(ROOT."/debug.log", $html);
+      $response->getBody()->write($html);
+      return $response->withHeader('Content-Type', 'text/html; charset=UTF-8');
+    });
+  });
 };
